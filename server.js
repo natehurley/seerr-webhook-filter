6b6c5a168bbe2a1ed24fe3dfed8e7e9fa9caa1d0
Nate Hurley <56522563+natehurley@users.noreply.github.com>
2026-03-19 21:24:16 -0500

    Update server port in console log message

--- server.js
+++ server.js
@@ -116,5 +116,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3456, () => {
-  console.log("🚀 Webhook server running on port 3000");
+  console.log("🚀 Webhook server running on port 3456");
 });
